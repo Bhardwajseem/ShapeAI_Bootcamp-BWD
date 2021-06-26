e19cd4dc9ffd6e35a11afd5dbdbc91dc45bc4171
Bhardwajseem <86520288+Bhardwajseem@users.noreply.github.com>
2021-06-26 17:37:51 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./Info";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <list />
+    </div>
+  );
+}
+export default App;
